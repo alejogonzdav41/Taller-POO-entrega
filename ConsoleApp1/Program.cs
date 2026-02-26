928c1820dc078f22a32ae8256f4b322112381095
tomas velez duque <tomyvelezd@gmail.com>
2026-02-25 20:47:13 -0500

    ava

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using GestionEmpleados;
+using System;
+using System.Collections.Generic;
+
+namespace GestionEmpleados
+{
+    public interface IMostrable
+    {
+        void MostrarInformacion();
+    }
+    public abstract class Empleado : IMostrable
+    {
+        private int id;
+        private string nombre;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public Empleado(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        public abstract double CalcularSalario();
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Nombre: " + Nombre);
+        }
+    }
+}
+
+class Program   
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
+
+
+
